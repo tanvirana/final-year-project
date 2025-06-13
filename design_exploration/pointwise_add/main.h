3838ab09ebfc2800307bb90165d9d8513590aaba
tanvirana <65024397+tanvirana@users.noreply.github.com>
2025-06-13 07:15:12 +0100

    Fix dummy call bit width in header

--- design_exploration/pointwise_add/main.h
+++ design_exploration/pointwise_add/main.h
@@ -7,5 +7,5 @@ ap_uint<IN_WIDTH + 1> bram_single_port_add(ap_uint<IN_WIDTH> in1, ap_uint<IN_WID
 ap_uint<IN_WIDTH + 1> bram_dual_port_add(ap_uint<IN_WIDTH> in1, ap_uint<IN_WIDTH> in2);
 ap_uint<IN_WIDTH + 1> lutram_single_port_add(ap_uint<IN_WIDTH> in1, ap_uint<IN_WIDTH> in2);
 ap_uint<IN_WIDTH + 1> lutram_dual_port_add(ap_uint<IN_WIDTH> in1, ap_uint<IN_WIDTH> in2);
-void calculate_value(ap_uint<IN_WIDTH> weights[N], ap_uint<IN_WIDTH> prev_in[N], ap_uint<32> result[N]);
+void calculate_value(ap_uint<IN_WIDTH> weights[N], ap_uint<IN_WIDTH> prev_in[N], ap_uint<IN_WIDTH + 1> result[N]);
 void example(hls::stream< ap_axis<32,2,5,6> > &A, hls::stream< ap_axis<32,2,5,6> > &B);
